bfc7cb1cd9dfed5d1d70a2dd0958866e4f6f731c
Marco V <marco.vidalonbaldeon@mail.mcgill.ca>
2023-03-10 15:47:26 -0500

    Update navigation.py
    Some code fixes & formatting

--- navigation.py
+++ navigation.py
@@ -1,16 +1,31 @@
-from utils.brick import EV3ColorSensor
+from utils.brick import EV3ColorSensor, wait_ready_sensors
 from math import sqrt
 
-class colorDetector:
+class ColorDetector:
+    """Class for the color detection algorithm.
+    """
 
-    COLORS = {}
+    #TODO: Set color value for each color
+    COLORS = {
+        "SAMPLE_NAME": [0,0,0]
+    }
     COLOR_TRESHOLD = 1
 
+    navSensor: EV3ColorSensor = None
+    delSensor: EV3ColorSensor = None
+
     def __init__(self, navigationPort, deliveryPort) -> None:
-        self.navSensor = EV3ColorSensor(navigationPort)
-        self.delSensor = EV3ColorSensor(deliveryPort)
+        """Constructor for the color_detector class.
+
+        Args:
+            navigationPort (_type_): _description_
+            deliveryPort (_type_): _description_
+        """
+        if (not self.navSensor): self.navSensor = EV3ColorSensor(navigationPort)
+        if (not self.delSensor): self.delSensor = EV3ColorSensor(deliveryPort)
+        wait_ready_sensors()
     
-    def get_color_name(self, rgb: list):
+    def getColorName(self, rgb: list):
         # normalize the values to be between 0 and 1
 
         ### UNIT-VECTOR METHOD ###
@@ -25,15 +40,19 @@ class colorDetector:
         normRBG = [red, green, blue]
 
         # compare normalized rbg to reference values
-        distances = {color: 0 for color in self.COLORS.keys()}
-        for color in self.COLORS.items():
-            difference = [color[1][0] - normRBG[0], color[1][0] - normRBG[1], color[1][2] - normRBG[2]]
-            distances[color[0]] = (sqrt(difference[0]**2 + difference[1]**2 + difference[2]**2))
-        minDistance = min(distances.values())
+        distances = {}
+        for colorName, colorRGB in self.COLORS.items():
+            difference = [colorRGB[0] - normRBG[0], 
+                          colorRGB[0] - normRBG[1], 
+                          colorRGB[2] - normRBG[2]]
+            distance = sqrt(difference[0]**2 + difference[1]**2 + difference[2]**2)
+            if distance <= self.COLOR_TRESHOLD:
+                distances[colorName] = distance
 
         # return detected color
-        if minDistance > self.colorTreshold:
+        if not distances:
             return "Unknown"
-        for color in distances.items():
-            if color[1] == minDistance:
-                return color[0]
+        minDistance = min(distances.values())
+        for colorName, distance in distances.items():
+            if distance == minDistance:
+                return colorName
