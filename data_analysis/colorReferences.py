5720ce72cca4f7f8d3cdd2946485a38fccb8ce4c
Marco V <marco.vidalonbaldeon@mail.mcgill.ca>
2023-03-10 22:05:45 -0500

    Color detection algorithm implementation

---
+++ data_analysis/colorReferences.py
@@ -0,0 +1,61 @@
+"""This module computes the mean & stev of the rgb values for each csv file and stores them in
+    ColorReferencesReport.txt while generating plots to visualize the results. This module uses
+    part of the starter code from lab 2.
+"""
+
+from ast import literal_eval
+from math import sqrt, e, pi
+from statistics import mean, stdev
+
+from matplotlib import pyplot as plt
+import numpy as np
+
+COLOR_SENSOR_DATA_FILES = ["blue.csv","green.csv","orange.csv","purple.csv","red.csv","white.csv","yellow.csv"]
+NBR_DECIMAL_DIGITS = 7
+
+color_report = ""
+
+def gaussian(x, values):
+    "Return a gaussian function from the given values."
+    sigma = stdev(values)
+    return (1 / (sigma * sqrt(2 * pi))) * e ** (-((x - mean(values)) ** 2) / (2 * sigma ** 2))
+
+for fileName in COLOR_SENSOR_DATA_FILES:
+    red, green, blue = [], [], []
+    with open(fileName, "r") as f:
+        for line in f.readlines():
+            r, g, b = literal_eval(line)  # convert string to 3 floats
+            # normalize the values to be between 0 and 1
+
+            ### UNIT-VECTOR METHOD ###
+            # denominator = sqrt(r ** 2 + g ** 2 + b ** 2)
+
+            ### RATIO METHOD ###
+            denominator = r + g + b
+            
+            red.append(r / denominator)
+            green.append(g / denominator)
+            blue.append(b / denominator)
+
+    color_report += f"File:\t{fileName}\n"
+    color_report += "Mean:\t"
+    color_report += f"r = {round(mean(red),NBR_DECIMAL_DIGITS)}\t"
+    color_report += f"g = {round(mean(green),NBR_DECIMAL_DIGITS)}\t"
+    color_report += f"b = {round(mean(blue),NBR_DECIMAL_DIGITS)}\n"
+    color_report += "Stdev:\t"
+    color_report += f"r = {round(stdev(red),NBR_DECIMAL_DIGITS)}\t"
+    color_report += f"g = {round(stdev(green),NBR_DECIMAL_DIGITS)}\t"
+    color_report += f"b = {round(stdev(blue),NBR_DECIMAL_DIGITS)}\n\n"
+
+    x_values = np.linspace(0, 1, 255)  # 255 evenly spaced values between 0 and 1
+    plt.figure(fileName, figsize=(8,5))
+    plt.plot(x_values, gaussian(x_values, red), color="r")
+    plt.plot(x_values, gaussian(x_values, green), color="g")
+    plt.plot(x_values, gaussian(x_values, blue), color="b")
+    plt.xlabel("Normalized intensity value")
+    plt.ylabel("Normalized intensity PDF by color")
+
+with open("colorReferencesReport.txt", "w") as f:
+    f.write(color_report)
+
+plt.show()
--- navigation.py
+++ navigation.py
@@ -2,30 +2,45 @@ from utils.brick import EV3ColorSensor, wait_ready_sensors
 from math import sqrt
 
 class ColorDetector:
-    """Class for the color detection algorithm.
-    """
+    """Class for the color detection algorithm."""
 
-    #TODO: Set color value for each color
     COLORS = {
-        "SAMPLE_NAME": [0,0,0]
+        "BLUE": [0.2309846, 0.4239539, 0.3450615],
+        "GREEN": [0.1969103, 0.6920012, 0.1110885],
+        "ORANGE": [0.693168, 0.252755, 0.054077],
+        "PURPLE": [0.7344298, 0.1123536, 0.1532166],
+        "RED": [0.784879, 0.1390653, 0.0760558],
+        "WHITE": [0.3760506, 0.4383683, 0.1855811],
+        "YELLOW": [0.4501223, 0.5066404, 0.0432373],
     }
-    COLOR_TRESHOLD = 1
+    COLOR_TRESHOLD = 0.3
 
     navSensor: EV3ColorSensor = None
     delSensor: EV3ColorSensor = None
 
-    def __init__(self, navigationPort, deliveryPort) -> None:
-        """Constructor for the color_detector class.
+    def __init__(self, navigationPort: int, deliveryPort: int) -> None:
+        """Constructor for the ColorDetector class.
 
         Args:
-            navigationPort (_type_): _description_
-            deliveryPort (_type_): _description_
+            navigationPort (int): The port of the navigation color sensor
+            deliveryPort (int): The port of the delivery color sensor
         """
         if (not self.navSensor): self.navSensor = EV3ColorSensor(navigationPort)
         if (not self.delSensor): self.delSensor = EV3ColorSensor(deliveryPort)
         wait_ready_sensors()
     
-    def getColorName(self, rgb: list):
+    def getColorName(self, rgb: list) -> str:
+        """Gets the color name from the given RGB values.
+        Returns "UNKNOWN" if no color is detected. Returns None if rgb is empty.
+
+        Args:
+            rgb (list): the RGB values of the color to be detected
+
+        Returns:
+            str: The name of the detected color.
+        """
+        if not any(rgb): return None
+
         # normalize the values to be between 0 and 1
 
         ### UNIT-VECTOR METHOD ###
@@ -51,8 +66,16 @@ class ColorDetector:
 
         # return detected color
         if not distances:
-            return "Unknown"
+            return "UNKNOWN"
         minDistance = min(distances.values())
         for colorName, distance in distances.items():
             if distance == minDistance:
                 return colorName
+            
+    def getNavSensorColor(self) -> str:
+        """Returns the name of the color detected by the navigation sensor."""
+        return self.getColorName(self.navSensor.get_rgb())
+    
+    def getDelSensorColor(self) -> str:
+        """Returns the name of the color detected by the delivery sensor."""
+        return self.getColorName(self.delSensor.get_rgb())
