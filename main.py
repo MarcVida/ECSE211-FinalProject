29d87ba206e5980ab4c53ce199ed32b5c3743427
Marco V <marco.vidalonbaldeon@mail.mcgill.ca>
2023-03-13 10:53:54 -0400

    Code organization

---
+++ main.py
@@ -0,0 +1,14 @@
+from navigation import Navigation
+from color_detection import ColorDetector
+from delivery import Delivery
+
+if __name__ == "__main__":
+    nav = Navigation()
+    deliv = Delivery()
+
+    """
+    1 - cube loading
+    2 - navigation sequence
+    3 - when stop reached:
+        a - 
+    """
