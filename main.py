9a5f9a8041ac5517b010d1585c75e7f38d6872aa
Marco V <marco.vidalonbaldeon@mail.mcgill.ca>
2023-03-13 11:04:34 -0400

    Update main.py
    System logic

--- main.py
+++ main.py
@@ -7,8 +7,25 @@ if __name__ == "__main__":
     deliv = Delivery()
 
     """
+    FIRST ITERATION:
     1 - cube loading
     2 - navigation sequence
-    3 - when stop reached:
-        a - 
+    3 - when stop reached, check case:
+        a - green stop:
+            1. save color & position to system
+            2. if correct color, drop cube
+        b - yellow stop: go to step 1
+    4 - go to step 2
+
+    NEXT ITERATIONS:
+    1 - cube loading
+    2 - navigation sequence
+    3 - when stop reached, check case:
+        a - green stop: 
+            1. track new position
+            2. if correct position:
+                a. drop cube
+                b. set direction towards next position (?)
+        b - yellow stop: go to step 1
+    4 - go to step 2
     """
