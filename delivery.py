4cc394de71b67fa89e7184ee06acbdcb038419d9
Marco V <marco.vidalonbaldeon@mail.mcgill.ca>
2023-03-13 12:58:31 -0400

    Update delivery

--- delivery.py
+++ delivery.py
@@ -1,23 +1,43 @@
 from utils.brick import Motor, TouchSensor
+from time import sleep
+
+def waitUntil(callback):
+    while not callback():
+        pass
 
 class Delivery:
 
     deliveryMotor: Motor
     loadingTS: TouchSensor
 
-    def __init__(self, deliveryPort: int, loadingPort: int) -> None:
+    def __init__(self, deliveryPort: int, loadingPort: int, debug: bool) -> None:
             self.deliveryMotor = Motor(deliveryPort)
             self.loadingTS = TouchSensor(loadingPort)
+            self.debug = debug
 
-    def loadingSequence():
-        pass
+    def loadingSequence(self):
+        # TODO: Implement this
+        self.log("Loading...")
+        waitUntil(self.isLoadingComplete)
+        self.log("Loading complete.")
 
-    def isLoadingComplete():
-        pass
+    def isLoadingComplete(self):
+        return self.loadingTS.is_pressed()
 
-    def deliverySequence():
-        pass
+    def deliverySequence(self):
+        # TODO: Implement this
+        self.log("Dropping cube...")
+        waitUntil(self.isDeliveryComplete)
+        self.log("Delivery complete.")
 
-    def isDeliveryComplete():
-        pass
+    def isDeliveryComplete(self):
+        # TODO: Implement this
+        return True
+
+    def log(self, message: str):
+        if self.debug: print(message)
     
+if __name__ == "__main__":
+    delivery = Delivery(0,0,True)
+    delivery.loadingSequence()
+    delivery.deliverySequence()
